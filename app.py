497c671e274e93c9a61bd12b9c0751f5db5a0cba
webtech-2024 <afolayandorcas46@gmail.com>
2025-08-29 12:57:05 -0700

    Fix host and port for Render deployment

--- app.py
+++ app.py
@@ -51,5 +51,7 @@ def index():
 
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
